294c92c114d3bdaf977a4c8fca7107408bd9a649
Пользователь <s4njar.azizov@gmail.com>
2023-08-09 19:58:08 +0600

    object

---
+++ style.js
@@ -0,0 +1,55 @@
+ //1 !)
+
+let user = {
+    name: "a",
+    age: 5,
+
+    name2: "b",
+    age2: 6,
+
+    name3: "c",
+    age3: 10,
+}
+
+alert(user.age2)
+
+console.log(user.age2)
+
+// 2)
+
+
+
+
+// 3 !)
+
+let array = [10, 15, 9, 10.]
+
+let result = 10 * 15 + 9 * 10
+
+alert(result)
+
+// 4 !)
+
+let arr = ['a', 'b', 'c', 'd']
+
+let result3 = 'a+b, c+d'
+
+alert(result3)
+
+
+// 5)
+
+const test = 'false'
+    
+
+if(test == true){
+    alert('Верно')
+}
+
+else {
+    alert('Неверно')
+}
+
+console.log(test)
+
+
